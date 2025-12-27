1bada0d3e5c77591dcaf066237a20843a555b2fb
Sai-Narayana <gadirajunarayana@gmail.com>
2025-12-27 15:09:57 +0530

    mongodb url correction

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://gjssai17_db_user:%24Srinivas%24%2617@tracker-database.bptzypx.mongodb.net/?appName=Tracker-database');
+    await mongoose.connect('mongodb+srv://gjssai17_db_user:0zMn39Yn1azcBWfd@tracker-database.bptzypx.mongodb.net/?appName=Tracker-database');
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
